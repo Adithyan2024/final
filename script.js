b9b45e307c5bd6f81e92d08a0f48e574c28a4736
sanju <sanjutl19@gmail.com>
2024-09-30 16:11:33 +0530

    sixth commit

--- script.js
+++ script.js
@@ -83,6 +83,20 @@ window.addEventListener('scroll', () => {
 
 
   document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
     let lastScrollY = window.scrollY;
     let scroll=770
     window.addEventListener('scroll', () => {
