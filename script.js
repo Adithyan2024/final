e7243fb11219a9a7531507481a3fcdcef8d63cfd
sanju <sanjutl19@gmail.com>
2024-09-30 18:43:32 +0530

    final  commit

--- script.js
+++ script.js
@@ -83,19 +83,19 @@ window.addEventListener('scroll', () => {
 
 
   document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector(".hamburger");
-  const navMenu = document.querySelector(".nav-menu");
+      const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
 
-  hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+  })
 
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+  }))
 
     let lastScrollY = window.scrollY;
     let scroll=770
